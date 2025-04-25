285e914e0009f1985baf499bce95a1dc8b2cb2d5
Luca-Lucaa <luca.luca@tutamail.com>
2025-04-25 20:33:29 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,34 @@
+// config.js
+export const USER_CREDENTIALS = {
+  Admin: "Admino25!",
+  Scholli: "Scholli25",
+  Jamaica05: "Werwer55",
+};
+
+export const USER_EMOJIS = {
+  Admin: "👑",
+  Scholli: "🚀",
+  Jamaica05: "🎩",
+};
+
+export const THEME_CONFIG = {
+  palette: {
+    primary: { main: "#3b82f6" },
+    secondary: { main: "#dc004e" },
+    background: { default: "#e0e7ff", paper: "#ffffff" },
+    text: { primary: "#333" },
+  },
+};
+
+export const GUIDES = [
+  { name: "Anleitung PlockTV", path: "/guides/PlockTV.pdf" },
+  { name: "Anleitung 2", path: "/guides/guide2.pdf" },
+];
+
+// Neue Zuordnung von Ersteller zu Hintergrundfarben (leichte Pastelltöne)
+export const OWNER_COLORS = {
+  Admin: "#f0f7ff",    // Hellblau
+  Scholli: "#f9f0ff",   // Helllila
+  Jamaica05: "#f5fff0", // Hellgrün
+  // Füge hier weitere Ersteller hinzu, falls nötig, mit passenden Farben
+};
